8a1cbb3bdb10ff2f7c7bb8aa086a9cae25e9524d
Ben Denzer <denzer.ben@gmail.com>
2019-10-27 18:10:32 -0700

    add gameboard type file

---
+++ src/types/Board.ts
@@ -0,0 +1,14 @@
+import { Card, Suit } from './Card';
+
+interface Foundation {
+  suit: Suit | null;
+  pile: Card[];
+}
+
+export interface Board {
+  stock: Card[];
+  waste: Card[];
+  foundations: Foundation[];
+  tableau: Card[][];
+  history: Board[];
+}
